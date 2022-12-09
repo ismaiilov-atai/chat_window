711378ff72c45d80f2fe93187e29fe3fdac70b7f
atai_ismaiilov <atai_ismaiilov@gmail.com>
2022-12-09 08:46:37 -0500

    removed all extra comments/cleaned up

--- chatWindow.js
+++ chatWindow.js
@@ -1,13 +1,10 @@
-/////////////////////////////* DATA *////////////////////////////
+
 let messageTime = new Date();
 
-////////////*  https://stackoverflow.com/users/3837522/abhay-kumar current time taken from abhy-kumar reply on stackoverflow */////////////
-// (2) Your messages should include date as well as time (currently you have only time).
+
 let currentTime = messageTime.toLocaleString('en-US', {day: '2-digit', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })
 
-/* (1). Responses from the computer should generate a random conversation, however,
- currently it always comes back with the same response “sender reply to come here”.
-  Add a bit of diversity to those responses. */
+
 let msgRandom = [
     {text: "Let's talk about getting new technologies to our project.", time: currentTime}, 
     {text: 'What do you think about Firebase integration in our authentication part?', time: currentTime},
@@ -27,15 +24,12 @@ $('#text').on('input', function() {
      this.value ? $('#btn_voice_send').text('send') : $('#btn_voice_send').text('mic');
 });
 
-/* (3) you should not be defining the onClick method on the button from HTML. Move the onClick logic to your .js file. */
+
 $('#btn_voice_send').click(function() {
     messageGenerater();
 });
 
-/*
-(4) Your .js file should interact with the DOM using only jQuery, but in a couple of places you are using native HTML DOM methods. 
-Everywhere you use the document keyword, you should replace it with the corresponding jQuery method.
-*/
+
 let displayingMessages = () => {
     
     for (let i = 0; i < toDisplayMsg.length; i++) {
